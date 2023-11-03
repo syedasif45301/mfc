c12e2ec447d27fb300eaa590e52eab386d64115f
syed asif <syedasif45301@gmail.com>
2023-11-03 22:03:07 +0530

    first commit

---
+++ maps.js
@@ -0,0 +1,61 @@
+/*
+let arr=['html','css','js']
+console.log(arr);
+const uppercase = arr.map((arr1) => arr1.toUpperCase());
+
+console.log(uppercase);
+
+
+let arr2=[1,2,3,4,5];
+console.log(arr2);
+const numbers=arr2.map((arr3) => arr3*arr3);
+console.log(numbers);
+
+
+let arr4=[
+    item1={
+        product:'shoe',price:34,
+
+    },
+
+    item2={
+        product:'tshirt',price:38,
+    },
+
+    
+    item3={
+        product:'watch',price:32,
+    }
+]
+console.log(arr4)
+
+const pro=arr4.map((ponly) => ponly.product);
+console.log(pro); 
+
+let celsius=[24,28,30,36,38]
+console.log("celsius:",celsius)
+let fahrenheit=celsius.map((ctof) => (ctof * 9/5) + 32);
+console.log("fahrenheit:",fahrenheit); 
+
+let students=[
+
+    stu1={
+        name:'rahul',age:22
+    },
+
+    stu2={
+        name:'vijay',age:21
+    },
+    stu3={
+        name:'kohli',age:24
+    },
+    stu4={
+        name:'iyer',age:23
+    },
+]
+console.log(students);
+let abc=students.map((xyz)=> (
+{name: xyz.name, YoB:2023-xyz.age}));
+console.log(abc)
+
+*/
